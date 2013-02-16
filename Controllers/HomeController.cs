6059561e1485095b18bd869ce35f9fb61d2e0441
Christopher Harris <xixonia@gmail.com>
2013-02-16 16:54:10 -0600

    First Commit!

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var christopher = new Person("Christopher", DateTime.Parse("11/11/1989"));
+
+            return this.View(christopher);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MvcApplication1.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person(string name, DateTime dateOfBirth)
+        {
+            this.Name = name;
+            this.DateOfBirth = dateOfBirth;
+        }
+    }
+}
